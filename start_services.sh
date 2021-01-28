ebc58e1977964c70d4d72bace4fde6df8dee2f3b
dung.v.nguyen <dung.v.nguyen@taptap.com.vn>
2021-01-29 03:53:06 +0700

    update scr 1

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,5 @@
 cd /app
-ls
+ls -a
 # Start rasa server with nlu model
 echo 'hello'
-rasa run --credentials credentials.yml --model models --enable-api --cors '*' --debug -p $PORT
+rasa run --credentials /app/credentials.yml --model models --enable-api --cors '*' --debug -p $PORT
